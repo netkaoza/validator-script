c4c8fed67c50a638d075683b6bf45793c3a5585c
atchapcyp <atcha.crypto@gmail.com>
2020-07-16 23:54:07 +0700

    🌈 Add EWC and  Volta running validator lookup script

---
+++ ewc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {1..100};
+do 
+echo ''
+curl -s --data '{"method":"eth_getBlockByNumber","params":["latest",true],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST https://rpc.energyweb.org | jq .result.extraData | xxd -p -r ; sleep 3; 
+done
+
